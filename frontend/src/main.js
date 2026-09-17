e900b343fbd7de32f9f5fec10f535bedd7e209c5
Josue <josuefox123@users.noreply.github.com>
2026-09-17 14:22:30 +0100

    fix: chargement de la feuille de style bootstrap-icons pour l'affichage de toutes les icônes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-// Importation de Bootstrap CSS et JS
+// Importation de Bootstrap CSS, JS et Bootstrap Icons
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Styles CSS globaux et personnalisés
